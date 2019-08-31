d404b3e8cd31facf29015c90676d1728303ac048
Ajit Jadhav <mr.ajitjadhav@gmail.com>
2019-08-31 15:52:38 +0530

    printf updated

--- producer_consumer_bb/main.c
+++ producer_consumer_bb/main.c
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[])
     pthread_attr_init(&attr[1]);
     pthread_create(&tid[0], &attr[0], producer, NULL);
     pthread_create(&tid[1], &attr[1], consumer, NULL);
-    printf("Two threads with ID's %lu & %lu created.\n", tid[0], tid[1]);
+    printf("Producer with ID's %lu & consumer with ID %lu created.\n", tid[0], tid[1]);
     sleep(30);
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
